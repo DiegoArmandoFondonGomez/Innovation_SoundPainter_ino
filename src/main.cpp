6cc18d6e3513c715334e9dc0ad33ab24e2a982f0
DiegoArmandoFondon <a01199394@tec.mx>
2023-11-11 14:27:02 -0600

    Example Sound Level Sketch

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
-#include <Arduino.h>
+/****************************************
+Example Sound Level Sketch for the 
+Adafruit Microphone Amplifier
+****************************************/
 
-// put function declarations here:
-int myFunction(int, int);
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+   Serial.begin(9600);
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+   unsigned long startMillis = millis();  // Start of sample window
+   unsigned int peakToPeak = 0;   // peak-to-peak level
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+   unsigned int signalMax = 0;
+   unsigned int signalMin = 1024;
+
+   // collect data for 50 mS
+   while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(0);
+      if (sample < 1024)  // toss out spurious readings
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;  // save just the max levels
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;  // save just the min levels
+         }
+      }
+   }
+   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+   double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
+
+   Serial.println(volts);
 }
